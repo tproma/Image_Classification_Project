504d69bcd4b43c6f39ba6cbe9694f50caaed9c57
Tanjina <tanjinaproma@gmail.com>
2023-07-22 11:32:33 +0600

    utils modified

--- src/ImageClassifier/utils/common.py
+++ src/ImageClassifier/utils/common.py
@@ -91,3 +91,18 @@ def load_bin(path: Path) -> Any:
     data = joblib.load(path)
     logger.info(f"binary file loaded from: {path}")
     return data
+
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
