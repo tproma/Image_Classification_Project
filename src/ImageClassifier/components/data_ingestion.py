8fa9ba2596718cc3c1d1a90cf67b23d796d4c658
Tanjina <tanjinaproma@gmail.com>
2023-07-29 11:35:48 +0600

    ingestion component modified

--- src/ImageClassifier/components/data_ingestion.py
+++ src/ImageClassifier/components/data_ingestion.py
@@ -3,3 +3,9 @@ import urllib.request as request
 import zipfile
 from ImageClassifier import logger
 from ImageClassifier.utils.common import get_size
+from ImageClassifier.entity.config_entity import DataIngestionConfig
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
