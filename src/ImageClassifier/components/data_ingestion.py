758276528416c84631247b2386a897b29f905942
Tanjina <tanjinaproma@gmail.com>
2023-07-30 20:21:53 +0600

    ingestion component modified

--- src/ImageClassifier/components/data_ingestion.py
+++ src/ImageClassifier/components/data_ingestion.py
@@ -9,3 +9,13 @@ class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
 
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                url = self.config.source_URL,
+                filename = self.config.local_data_file
+            )
+            logger.info(f"{filename} download! with following info: \n{headers}")
+        else:
+            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")  
