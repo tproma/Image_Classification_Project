f60b889457ac262e0f6b83fdafb5d124af4ad760
Tanjina <tanjinaproma@gmail.com>
2023-07-30 20:22:32 +0600

    ingestion component modified

--- src/ImageClassifier/components/data_ingestion.py
+++ src/ImageClassifier/components/data_ingestion.py
@@ -19,3 +19,15 @@ class DataIngestion:
             logger.info(f"{filename} download! with following info: \n{headers}")
         else:
             logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")  
+
+
+    def extract_zip_file(self):
+        """
+        zip_file_path: str
+        Extracts the zip file into the data directory
+        Function returns None
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
