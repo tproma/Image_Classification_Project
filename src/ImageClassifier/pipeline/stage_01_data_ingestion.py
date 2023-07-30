114f1f5b81dfc239e20300151cabc334121620c4
Tanjina <tanjinaproma@gmail.com>
2023-07-30 20:29:48 +0600

    ingestion pipeline modified

--- src/ImageClassifier/pipeline/stage_01_data_ingestion.py
+++ src/ImageClassifier/pipeline/stage_01_data_ingestion.py
@@ -2,3 +2,6 @@ from ImageClassifier.config.configuration import ConfigurationManager
 from ImageClassifier.components.data_ingestion import DataIngestion
 from ImageClassifier import logger
 
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
