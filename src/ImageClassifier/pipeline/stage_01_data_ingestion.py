8b2e4d6ad614a916656c505b415c7f1a26936c78
Tanjina <tanjinaproma@gmail.com>
2023-07-31 15:12:57 +0600

    ingestion pipeline modified

--- src/ImageClassifier/pipeline/stage_01_data_ingestion.py
+++ src/ImageClassifier/pipeline/stage_01_data_ingestion.py
@@ -2,6 +2,7 @@ from ImageClassifier.config.configuration import ConfigurationManager
 from ImageClassifier.components.data_ingestion import DataIngestion
 from ImageClassifier import logger
 
+STAGE_NAME = "Data Ingestion Stage"
 class DataIngestionTrainingPipeline:
     def __init__(self):
         pass
