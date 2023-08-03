96a1e6d785bddca5e3732b7d47cdc64c24799646
Tanjina <tanjinaproma@gmail.com>
2023-08-03 09:55:44 +0600

    ingestion pipeline modified

--- src/ImageClassifier/pipeline/stage_01_data_ingestion.py
+++ src/ImageClassifier/pipeline/stage_01_data_ingestion.py
@@ -18,8 +18,10 @@ class DataIngestionTrainingPipeline:
 if __name__ == "__main__":
     try:
         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>stage {STAGE_NAME} completed <<<<<<")
     except Exception as e:
         logger.exception(e)
         raise e
-    
     
