05b57ea5c831da14961b1ec67a295cb84398bd8b
Tanjina <tanjinaproma@gmail.com>
2023-08-04 19:37:22 +0600

    main.py modified

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from ImageClassifier import logger
 from ImageClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
