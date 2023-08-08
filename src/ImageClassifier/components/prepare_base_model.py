9487dd18661ac755df192e0dc43f327fde9722eb
Tanjina <tanjinaproma@gmail.com>
2023-08-08 09:50:07 +0600

    prepare basemodel component modified

--- src/ImageClassifier/components/prepare_base_model.py
+++ src/ImageClassifier/components/prepare_base_model.py
@@ -2,6 +2,9 @@ import os
 import urllib.request as request
 from zipfile import ZipFile
 import tensorflow as tf
+from pathlib import Path
+from ImageClassifier.entity.config_entity import PrepareBaseModelConfig
+
 
 class PrepareBaseModel:
     def __init__(self, config: PrepareBaseModelConfig):
