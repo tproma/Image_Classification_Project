6dd115cdc4393e6fc3b9ffb3dbceed9e48f8c118
Tanjina <tanjinaproma@gmail.com>
2023-08-09 21:13:58 +0600

    prepare base model training pipeline modified

--- src/ImageClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/ImageClassifier/pipeline/stage_02_prepare_base_model.py
@@ -15,3 +15,16 @@ class PrepareBaseModelTrainingPipeline:
         prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
         prepare_base_model.get_base_model()
         prepare_base_model.update_base_model()
+
+
+    
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
