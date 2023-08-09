b5b77ea34ee33b2ed204334a49b1161db3af44f3
Tanjina <tanjinaproma@gmail.com>
2023-08-09 21:10:41 +0600

    prepare base model training pipeline modified

--- src/ImageClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/ImageClassifier/pipeline/stage_02_prepare_base_model.py
@@ -7,3 +7,11 @@ STAGE_NAME = "Prepare base model"
 class PrepareBaseModelTrainingPipeline:
     def __init__(self):
         pass
+
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
