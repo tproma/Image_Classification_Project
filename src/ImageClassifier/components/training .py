0c777a0bdc0e58a002049dfe445190501216ee3f
Tanjina <tanjinaproma@gmail.com>
2023-08-23 09:45:47 +0600

    training component modified

--- src/ImageClassifier/components/training .py
+++ src/ImageClassifier/components/training .py
@@ -2,3 +2,13 @@ from ImageClassifier.entity.config_entity import TrainingConfig
 import tensorflow as tf
 from pathlib import Path
 
+
+class Training:
+    def __init__(self, config: TrainingConfig):
+        self.config = config
+    
+    def get_base_model(self):
+        self.model = tf.keras.models.load_model(
+            self.config.updated_base_model_path
+        )
+    
