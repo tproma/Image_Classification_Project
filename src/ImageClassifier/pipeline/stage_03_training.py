98acf4de745dfb76ee1fce21637e53b1e269b900
Tanjina <tanjinaproma@gmail.com>
2023-08-24 00:16:52 +0600

    training pipeline added

---
+++ src/ImageClassifier/pipeline/stage_03_training.py
@@ -0,0 +1,6 @@
+from ImageClassifier.config.configuration import ConfigurationManager
+from ImageClassifier.components.prepare_callbacks import PrepareCallback
+from ImageClassifier.components.training import Training 
+
+
+
