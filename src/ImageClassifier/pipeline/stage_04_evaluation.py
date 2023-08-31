fb36eab02301b87e7840077d4d636e97ca8f45aa
Tanjina <tanjinaproma@gmail.com>
2023-08-31 21:32:55 +0600

    Evaluation Component added

--- src/ImageClassifier/pipeline/stage_04_evaluation.py
+++ src/ImageClassifier/pipeline/stage_04_evaluation.py
@@ -15,3 +15,17 @@ class EvaluationPipeline:
         evaluation = Evaluation(val_config)
         evaluation.evaluation()
         evaluation.save_score()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+            
