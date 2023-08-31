82d50858c8d34a7d6e0b90aa7296438c2f6e5ff3
Tanjina <tanjinaproma@gmail.com>
2023-08-31 20:41:35 +0600

    Evaluation Config added

--- src/ImageClassifier/config/configuration.py
+++ src/ImageClassifier/config/configuration.py
@@ -87,3 +87,14 @@ class ConfigurationManager:
 
         return training_config
     
+
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chicken-fecal-images",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
