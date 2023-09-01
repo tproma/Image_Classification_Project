c8431affab65930787512b0d090f0036d664b248
Tanjina <tanjinaproma@gmail.com>
2023-09-01 06:36:10 +0600

    evaluation stage added in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ImageClassifier import logger
 from ImageClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ImageClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from ImageClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from ImageClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -36,3 +37,18 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+
+STAGE_NAME = "Evaluation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>>stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
