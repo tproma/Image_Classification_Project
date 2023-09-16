93fbe063a33ee52f2f1d2064021a71980dc2af23
Tanjina <tanjinaproma@gmail.com>
2023-09-16 16:31:24 +0600

    class added

--- src/ImageClassifier/pipeline/predict.py
+++ src/ImageClassifier/pipeline/predict.py
@@ -1,3 +1,8 @@
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+import os
+
+class PredictionPipeline:
+    def __init__(self, filename):
+        self.filename = filename
