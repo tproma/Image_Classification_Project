ba4f9e2846e95388dac37bcae7a4fdef879a3ce5
Tanjina <tanjinaproma@gmail.com>
2023-09-18 19:08:52 +0600

    predict function modified

--- src/ImageClassifier/pipeline/predict.py
+++ src/ImageClassifier/pipeline/predict.py
@@ -11,3 +11,6 @@ class PredictionPipeline:
     def predict(self):
         #load model
         model = load_model(os.path.join("artifacts", "training", "model.h5"))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
