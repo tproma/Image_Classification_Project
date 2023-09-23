306ac0c82dd03465472313e2f2a9051fdbf6ba9f
Tanjina <tanjinaproma@gmail.com>
2023-09-24 00:21:02 +0600

    functions added

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
 from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/via_postman', methods= ['POST'])
+def math_postman():
+    if (request.method =='POST'):
+        n1 = int(request.json['n1'])
+        n2 = int(request.json['n2'])
+        n3 = int(request.json['n3'])
+        result = n1 * n2 *n3
+        return jsonify(result)
+    
+
+
+@app.route('/pro', methods= ['POST'])
+def strs():
+    if (request.method =='POST'):
+        name  = request.json['name']
+        email = request.json['email']
+        
+        return jsonify(name + email)
+
+if __name__=='__main__':
+    app.run()
