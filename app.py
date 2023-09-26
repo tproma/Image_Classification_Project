dfd621136d1c6753aba93b05fb963566d30980ca
Tanjina <tanjinaproma@gmail.com>
2023-09-26 11:22:45 +0600

    app.py modified

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template 
+import os
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
